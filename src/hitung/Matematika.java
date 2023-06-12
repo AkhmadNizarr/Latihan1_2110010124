025fe0817d6ac6ff341c5764db7a7e1c05d4ca5a
AkhmadNizarr <nizarbjb87@gmail.com>
2023-06-12 15:18:46 +0700

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,18 +1,30 @@
 package hitung;
 
 public class Matematika {
-//    membuat variabel 
+    //membuat variabel 
     private double bil1,bil2;
 
-//    membuat Constructor pakai Parameter
+    //membuat Constructor pakai Parameter
     public Matematika(double bil1, double bil2) {
         this.bil1 = bil1;
         this.bil2 = bil2;
     }
     
-//    membuat method setPenjumlahan
+    //membuat method setPenjumlahan
     public double setPenjumlahan (){
         return bil1 + bil2;
+    }
+    //membuat method setPengurangan
+     public double setPengurangan(){
+        return bil1 - bil2;
+    }
+    //membuat method setPerkalian
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    //membuat method setPembagian
+    public double setPembagian(){
+        return bil1 / bil2;
     }
     
     
